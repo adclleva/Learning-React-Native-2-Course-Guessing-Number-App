2cb3156195313aa7a9c0da7b1149d997b5ea1d5e
Arvin Lleva <arvin.lleva@newrocket.com>
2020-05-28 00:19:46 -0700

    Utilized the special properties of having a text component within a text component

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
 
 const GameOverScreen = (props) => {
   const { roundsNumber, userNumber, onRestart } = props;
@@ -21,8 +22,13 @@ const GameOverScreen = (props) => {
           resizeMode="cover"
         />
       </View>
-      <BodyText>Number of rounds: {roundsNumber}</BodyText>
-      <BodyText>Number was: {userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{" "}
+          <Text style={styles.hightlight}>{roundsNumber}</Text> rounds to guess
+          the number <Text style={styles.hightlight}>{userNumber}</Text>
+        </BodyText>
+      </View>
       <Button title="NEW GAME" onPress={onRestart} />
     </View>
   );
@@ -50,6 +56,18 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  hightlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20,
   },
 });
 
