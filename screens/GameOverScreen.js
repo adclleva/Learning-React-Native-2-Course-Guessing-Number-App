6751dbcb377dfafd2a9f1305ade6ef652da4bfc8
Arvin Lleva <arvin.lleva@newrocket.com>
2020-05-28 00:06:55 -0700

    able to get an image through the web

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -8,7 +8,19 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
-      <Image source={require("../assets/success.png")} />
+      <View style={styles.imageContainer}>
+        <Image
+          // the fade-in effect is ued for all images when loading
+          fadeDuration={100}
+          // source={require("../assets/success.png")}
+          source={{
+            uri:
+              "https://cdn.pixabay.com/photo/2019/01/22/18/30/summit-3948706_960_720.jpg",
+          }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
       <BodyText>Number of rounds: {roundsNumber}</BodyText>
       <BodyText>Number was: {userNumber}</BodyText>
       <Button title="NEW GAME" onPress={onRestart} />
@@ -21,6 +33,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    /**
+     * having the same height and width and also halving the borderRadius
+     *
+     */
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    width: 300,
+    height: 300,
+    overflow: "hidden", // The overflow is clipped, the rest of the content will be invisible
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
